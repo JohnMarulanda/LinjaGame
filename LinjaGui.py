6742b69b0b2f430d5243ef15dbba6a771286fba8
ClusterMax <carlosguerrero0705@gmail.com>
2023-12-20 10:30:41 -0500

    GUI puede guardar matriz actual de juego en un archivo TXT

--- LinjaGui.py
+++ LinjaGui.py
@@ -38,6 +38,23 @@ subTurn = 1
 movements = 1 #Numero de movimientos
 
 
+def guardar_archivo(matrix):
+    root = Tk()
+    root.withdraw()
+
+    # Seleccionar la carpeta de destino
+    folder_selected = filedialog.askdirectory()
+
+    if folder_selected:
+        file_path = folder_selected + '/matrix.txt'  # Ruta para el archivo en la carpeta seleccionada
+
+        # Guardar la matriz en un archivo de texto
+        with open(file_path, 'w') as file:
+            for row in matrix:
+                file.write(' '.join(map(str, row)) + '\n')
+
+        print(f"La matriz se ha guardado en {file_path}")
+
 def cargar_archivo():
     root = Tk()
     root.withdraw()  # Evita que se abra una ventana principal de Tkinter
@@ -103,9 +120,9 @@ matrix_rows = len(matrix)
 matrix_cols = len(matrix[0])
 
 # Ancho de la ventana
-window_width = 800
+window_width = 1000
 # Ancho deseado para el juego
-desired_game_width = 600
+desired_game_width = 800
 
 # Calcula el ancho de la celda basado en el ancho deseado del juego
 cell_width = desired_game_width // matrix_cols
@@ -123,9 +140,13 @@ blank_surface.fill((45, 87, 44))  # Rellena el área en blanco con blanco
 
 # Coordenadas y tamaño del nuevo botón (debajo del botón existente)
 two_button_rect = pygame.Rect(
-    600, 200, 160, 70
-)  #  Puedes ajustar las coordenadas y el tamaño según tus necesidades
+    820, 180, 160, 70
+)
 
+# Coordenadas y tamaño del nuevo botón (debajo del botón existente)
+three_button_rect = pygame.Rect(
+    820, 320, 160, 70
+)
 
 text_rect = pygame.Rect(
     20, 50, 160, 40
@@ -135,9 +156,18 @@ text_rect = pygame.Rect(
 button_unpressed_image = pygame.image.load("Sprites/Play_Unpressed.png")
 button_pressed_image = pygame.image.load("Sprites/Play_Pressed.png")
 
+
+# Cargar las imágenes del botón guardar
+button_save_unpressed_image = pygame.image.load("Sprites/Save_Unpressed.png")
+button_save_pressed_image = pygame.image.load("Sprites/Save_Pressed.png")
+
 # Escalar las imágenes al tamaño deseado (ancho, alto)
 button_unpressed_image = pygame.transform.scale(button_unpressed_image, (160, 70))
 button_pressed_image = pygame.transform.scale(button_pressed_image, (160, 70))
+
+# Escalar las imágenes de guardar al tamaño deseado (ancho, alto)
+button_save_unpressed_image = pygame.transform.scale(button_save_unpressed_image, (160, 70))
+button_save_pressed_image = pygame.transform.scale(button_save_pressed_image, (160, 70))
 
 # Coordenadas donde dibujar la imagen del botón
 button_rect = button_unpressed_image.get_rect()
@@ -145,6 +175,13 @@ button_rect.topleft = (20, 180)  # Ajusta las coordenadas según tus necesidades
 
 # Estado actual del botón
 button_pressed = False
+
+# Coordenadas donde dibujar la imagen del botón guardar
+button_save_rect = button_save_unpressed_image.get_rect()
+button_save_rect.topleft = (20, 320)  # Ajusta las coordenadas según tus necesidades
+
+# Estado actual del botón
+button_save_pressed = False
 
 # Crear una fuente para el texto
 font = pygame.font.Font("Sprites/PokemonGb-RAeo.ttf", 12)  # Tamaño de la fuente
@@ -194,6 +231,14 @@ while running:
                 pygame.time.set_timer(
                     pygame.USEREVENT, 200
                 )  # 200 ms (ajusta según tu preferencia)
+            elif three_button_rect.collidepoint(event.pos):
+                guardar_archivo(matrix)
+                # Cambia el estado del botón
+                button_save_pressed = True
+                # Espera un tiempo antes de restaurar el botón a su estado normal
+                pygame.time.set_timer(
+                    pygame.USEREVENT, 200
+                )  # 200 ms (ajusta según tu preferencia)
             else:
                 # Obtener las coordenadas al hacer clic en una celda
                 row, col = obtener_coordenadas(event.pos)
@@ -219,6 +264,7 @@ while running:
         if event.type == pygame.USEREVENT:
             # Restaura el botón a su estado normal
             button_pressed = False
+            button_save_pressed = False
 
     # Dibuja la superficie degradada en blank_surface con un modo de mezcla
     blank_surface.fill((0, 0, 0))
@@ -245,13 +291,20 @@ while running:
     else:
         blank_surface.blit(button_unpressed_image, button_rect)
 
+    if button_save_pressed:
+        blank_surface.blit(button_save_pressed_image, button_save_rect)
+    else:
+        blank_surface.blit(button_save_unpressed_image, button_save_rect)
+
     # Renderizar el texto centrado en el rectángulo del texto
     text1 = large_font.render("Linja", True, (0, 0, 0))
     text2 = font.render("Load a Game", True, (0, 0, 0))
     text3 = font.render("Estrategic game", True, (0, 0, 0))
+    text_save = font.render("Save game", True, (0, 0, 0))
     blank_surface.blit(text1, text_rect.move(25, 10))
     blank_surface.blit(text2, text_rect.move(18, 100))
     blank_surface.blit(text3, text_rect.move(-5, 50))
+    blank_surface.blit(text_save, text_rect.move(27, 245))
 
     # Pruebas de muestra
     #text4 = font.render("Ganador: IA", True, (0, 0, 0))
@@ -259,16 +312,23 @@ while running:
 
     # Llamamos a la función de calcular puntajes
     red_score, black_score, winner = controller.calculate_scores(matrix)
+    winner_label = ""
+
+    # Bucle para cambiar el ganador (int) a string entendible por el usuario
+    if (winner == 1):
+        winner_label = "Red"
+    else:
+        winner_label = "Black"
 
     # Renderizar textos fuera del bucle de eventos
-    text_winner = font.render(f"Winner: {winner}", True, (0, 0, 0))
-    blank_surface.blit(text_winner, (18, 300))
+    text_winner = font.render(f"Winner: {winner_label}", True, (0, 0, 0))
+    blank_surface.blit(text_winner, (18, 450))
 
     text_red_score = font.render(f"Red: {red_score}", True, (0, 0, 0))
-    blank_surface.blit(text_red_score, (18, 350))
+    blank_surface.blit(text_red_score, (18, 500))
 
     text_black_score = font.render(f"Black: {black_score}", True, (0, 0, 0))
-    blank_surface.blit(text_black_score, (18, 400))
+    blank_surface.blit(text_black_score, (18, 550))
 
     #text5 = font.render("Puntaje: 33", True, (0, 0, 0))
     #blank_surface.blit(text5, text_rect.move(18, 320))
